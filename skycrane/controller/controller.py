6bdf8bc05bda297a5a11d1edda7572c24aef3ab0
Chetan Sharma <ban@mit.edu>
2019-02-08 20:51:19 -0500

    Added controller busy-wait to avoid overflowing planning buffer

--- skycrane/controller/controller.py
+++ skycrane/controller/controller.py
@@ -4,12 +4,15 @@ from serial import Serial
 
 class TwitchController():
     def __init__(self):
-        self.grbl = Serial(GRBL_PORT, GRBL_BAUD, timeout=1)
+        self.grbl = Serial(GRBL_PORT, GRBL_BAUD, timeout=3)
         for command in GRBL_CONFIG:
             self.send(command)
 
     def send(self, command):
         self.grbl.write((command + "\r\n").encode())
+        # intentionally blocking to rate-limit
+        if self.grbl.readline() != "ok":
+            raise IOError("Grbl reported error!")
 
     def do(self, command, value):
         if command == Command.LEFT:
