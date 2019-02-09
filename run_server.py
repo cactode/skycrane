012c1743855c546fe0896a862f7c11a3f115e1f7
Chetan Sharma <ban@mit.edu>
2019-02-08 20:05:24 -0500

    Fixed indentation errors

--- run_server.py
+++ run_server.py
@@ -4,13 +4,14 @@ from time import sleep
 
 failures = 0
 while failures < 5:
-	try:
-		crane = SkyCrane()
-		while True:
-			crane.run()
-			sleep(1)
-	except Exception as e:
-		failures += 1
-		print("System crashed!")
-		print(e)
-		print("Attempting restart #", failures)
+    try:
+        crane = SkyCrane()
+        while True:
+            crane.run()
+            sleep(1)
+    except Exception as e:
+        failures += 1
+        print("System crashed!")
+        print(e)
+        print("Attempting restart #", failures)
+print("Giving up. Goodbye!")
