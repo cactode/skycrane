5f1aff03b380194f87d16509b1c7dc3089f61f55
Chetan Sharma <ban@mit.edu>
2019-02-10 11:47:17 -0500

    Added code for winch

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # skycrane
 A small program to control a very large claw machine over Twitch
-
-plan:
-twitch returns list of string inputs
-
-parser reads in list of string inputs and translates to commands.
-	could be decided by votes or just randomly pick one
-	second is probably better
-
-controller reads in list of commands and translates into movements.
--- skycrane/controller/controller.py
+++ skycrane/controller/controller.py
@@ -1,5 +1,7 @@
 from config import Command, GRBL_CONFIG, GRBL_PORT, GRBL_BAUD
 from serial import Serial
+import pigpio
+from threading import Timer
 from time import sleep
 
 
@@ -13,6 +15,10 @@ class TwitchController():
         for command in GRBL_CONFIG:
             self.send(command)
 
+        pi = pigpio.pi()
+        pi.set_mode(3, pigpio.OUTPUT)
+        pi.stop()
+
     def send(self, command):
         self.grbl.write((command + "\n").encode())
         # intentionally blocking to rate-limit
@@ -21,6 +27,18 @@ class TwitchController():
             raise IOError("Grbl error! Responded with: " +
                           (response or "nothing."))
         print("Sent command", command)
+
+    # value should be between -300 and 300
+    def pulse_motor(self, value):
+        pulse_length = abs(value)/100
+        pi = pigpio.pi()
+        pi.set_servo_pulsewidth(3, 500 if value < 0 else 1500)
+        Timer(pulse_length, self.turn_off_motor,
+              kwargs={'pi': pi}).start()
+
+    def turn_off_motor(self, pi):
+        pi.set_servo_pulsewidth(3, 1500)
+        pi.stop()
 
     def do(self, command, value):
         if command == Command.LEFT:
@@ -31,5 +49,9 @@ class TwitchController():
             self.send("G0 Y" + str(value))
         elif command == Command.DOWN:
             self.send("G0 Y" + str(-value))
+        elif command == Command.RAISE:
+            self.pulse_motor(value)
+        elif command == Command.LOWER:
+            self.pulse_motor(-value)
         else:
             raise ValueError("Unrecognized command")
